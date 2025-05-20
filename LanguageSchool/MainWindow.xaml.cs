6c411b7d5a4819d313baf740f091daec26161073
kiram <kiram@DESKTOP-SCLS8RD>
2025-05-20 16:39:04 +0500

    Добавьте файлы проекта.

---
+++ LanguageSchool/MainWindow.xaml.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace LanguageSchool
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+        private void VhodButtonClick(object sender, RoutedEventArgs e)
+        {
+            string phone = PhoneBox.Text;
+            string password = PasswordBox.Password;
+
+            if (phone.Length < 5)
+            {
+                PhoneBox.ToolTip = "Данные заполнены некорректно!";
+                PhoneBox.Background = Brushes.Gray;
+            }
+            else if (password.Length < 3)
+            {
+                PasswordBox.ToolTip = "Данные заполнены некорректно!";
+                PasswordBox.Background = Brushes.Gray;
+            }
+            else
+            {
+                PhoneBox.ToolTip = "";
+                PhoneBox.Background = Brushes.Transparent;
+                PasswordBox.ToolTip = "";
+                PasswordBox.Background = Brushes.Transparent;
+                MessageBox.Show("Alright!");
+            }
+
+        }
+    }
+}
---
+++ LanguageSchool/Model/Core.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LanguageSchool.Model
+{
+    public class Core
+    {
+        /// <summary>
+        /// Поле _context хранит ссылку на экземпляр контекста базы данных LanguageSchoolDBEntities.
+        /// Это поле используется для взаимодействия с базой данных.
+        /// </summary>
+        private readonly LanguageSchoolDBEntities _context;
+
+        /// <summary>
+        /// Конструктор класса Core инициализирует экземпляр класса с указанным контекстом базы данных.
+        /// Если переданный контекст равен null, выбрасывается исключение ArgumentNullException.
+        /// </summary>
+        /// <param name="context">Экземпляр контекста базы данных LanguageSchoolDBEntities.</param>
+        /// <exception cref="ArgumentNullException">Выбрасывается, если переданный контекст равен null.</exception>
+        public Core(LanguageSchoolDBEntities context)
+        {
+            _context = context ?? throw new ArgumentNullException(nameof(context));
+        }
+
+        /// <summary>
+        /// Свойство Context предоставляет доступ к текущему контексту базы данных.
+        /// Оно возвращает экземпляр LanguageSchoolDBEntities, который был передан в конструктор.
+        /// </summary>
+        public LanguageSchoolDBEntities Context => _context;
+    }
+}
